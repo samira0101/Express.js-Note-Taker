9a90e27bb8f4ee0225cfa73958312093f9ad1b44
Allure <samira-h@hotmail.co.uk>
2022-06-04 14:04:30 +0100

    POST /api/notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,3 +13,24 @@ module.exports = (app) => {
   app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'));
   });
+
+  // POST /api/notes should receive a new note to save on the request body, 
+  // add it to the db.json file, and then return the new note to the client. 
+  app.post('/api/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db);
+    // creating body for note
+    let userNote = {
+      title: req.body.title,
+      text: req.body.text,
+      // creating unique id for each note
+      id: uniqid(),
+    };
+    // pushing created note to be written in the db.json file
+    db.push(userNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+    res.json(db);
+
+  });
+
