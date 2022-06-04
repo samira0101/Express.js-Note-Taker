6fc86aa55e5803e8b1bb562a24fdf3ce60e57a10
Allure <samira-h@hotmail.co.uk>
2022-06-04 14:01:14 +0100

    Sets the activeNote display/new note

--- public/assets/index.js
+++ public/assets/index.js
@@ -100,3 +100,23 @@ const handleNoteDelete = (e) => {
   });
 };
 
+// Sets the activeNote and displays it
+const handleNoteView = (e) => {
+  e.preventDefault();
+  activeNote = JSON.parse(e.target.parentElement.getAttribute('data-note'));
+  renderActiveNote();
+};
+
+// Sets the activeNote to and empty object and allows the user to enter a new note
+const handleNewNoteView = (e) => {
+  activeNote = {};
+  renderActiveNote();
+};
+
+const handleRenderSaveBtn = () => {
+  if (!noteTitle.value.trim() || !noteText.value.trim()) {
+    hide(saveNoteBtn);
+  } else {
+    show(saveNoteBtn);
+  }
+};
